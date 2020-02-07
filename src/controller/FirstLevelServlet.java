e5e4ed894b4eb58754fce1e6da0f64b59e1fd7a3
Bhuvana <bhuvanamjothivel@gmail.com>
2020-02-07 15:24:45 +0530

    Web Application based on String Method is done

---
+++ src/controller/FirstLevelServlet.java
@@ -0,0 +1,99 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.StringMethod;
+
+@WebServlet(urlPatterns = { "/firstlevel" })
+public class FirstLevelServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	StringMethod string = new StringMethod();
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		RequestDispatcher rs = this.getServletContext().getRequestDispatcher("/WEB-INF/views/firstlevel.jsp");
+		rs.forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String input = request.getParameter("input");
+		String lowerCase = request.getParameter("lowercase");
+		String upperCase = request.getParameter("uppercase");
+		String firstCharacter = request.getParameter("firstcharacter");
+		String length = request.getParameter("length");
+		String trim = request.getParameter("trim");
+		String seeAnswer = request.getParameter("seeanswer");
+		String secondLevel = request.getParameter("secondlevel");
+
+		string.setInput(input);
+
+		if (seeAnswer != null) {
+			if (lowerCase != null) {
+				String output = string.lower(input);
+				// System.out.println(output);
+				request.setAttribute("output", output);
+				RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/WEB-INF/views/firstlevel.jsp");
+				rd.forward(request, response);
+			}
+			/*
+			 * else { input = null; String output = string.lower(input);
+			 * //System.out.println(output); request.setAttribute("output", output);
+			 * RequestDispatcher rd=this.getServletContext().getRequestDispatcher(
+			 * "/WEB-INF/views/firstlevel.jsp"); rd.forward(request, response); }
+			 */
+
+		}
+
+		if (seeAnswer != null) {
+			if (upperCase != null) {
+				String output = string.upper(input);
+				request.setAttribute("output", output);
+				RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/WEB-INF/views/firstlevel.jsp");
+				rd.forward(request, response);
+			}
+		}
+
+		if (seeAnswer != null) {
+			if (firstCharacter != null) {
+				String output = string.firstCharacter(input);
+				request.setAttribute("output", output);
+				RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/WEB-INF/views/firstlevel.jsp");
+				rd.forward(request, response);
+			}
+		}
+
+		if (seeAnswer != null) {
+			if (length != null) {
+				String output = string.length(input);
+				request.setAttribute("output", output);
+				RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/WEB-INF/views/firstlevel.jsp");
+				rd.forward(request, response);
+			}
+		}
+
+		if (seeAnswer != null) {
+			if (trim != null) {
+				String output = string.trim(input);
+				request.setAttribute("output", output);
+				RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/WEB-INF/views/firstlevel.jsp");
+				rd.forward(request, response);
+			}
+		}
+
+		if (secondLevel != null) {
+			RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/WEB-INF/views/secondlevel.jsp");
+			rd.forward(request, response);
+		}
+
+	}
+
+}
